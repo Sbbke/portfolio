0e8755c680f75774f12b4fbecf487391c4b2a506
andrew <fghjklllll@gmail.com>
2026-04-03 10:29:31 -0500

    refactor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 
 export default defineConfig({
   base: "/portfolio/",
